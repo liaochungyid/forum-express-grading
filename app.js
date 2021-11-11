e1043038dfbf7da55c97e7fe2dcb999f18348bb2
Liao CY <liaochungyid@gmail.com>
2021-11-11 21:31:16 +0800

    copy all work have done.

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
+const db = require('./models')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extented: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
